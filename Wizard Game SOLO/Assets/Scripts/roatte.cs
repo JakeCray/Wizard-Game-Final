98c507b4890d934eb6c5b3d791b77591c1069687
Jake Cray <jake@Jakes-MacBook-Pro-2.local>
2015-04-04 12:35:39 +0100

    First Commit

---
+++ Wizard Game SOLO/Assets/Scripts/roatte.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roatte : MonoBehaviour {
+
+	public GameObject button;
+
+	public float time = 1f;
+
+	public float t = 1f;
+
+	private float final;
+
+	private float rotateFinal;
+
+	bool run;
+
+	RectTransform rt;
+
+	void Start()
+	{
+		final = time;
+		t = time;
+		button = gameObject;
+		rt = button.GetComponent<RectTransform> ();
+	}
+
+	public void rotate(float rotate)
+	{
+		run = true;
+		rotateFinal = rotate;
+	}
+
+	void Update()
+	{
+		if (run)
+		{
+			t -= Time.deltaTime;
+			rt.Rotate (Vector3.up, rotateFinal * Time.deltaTime);
+
+			if(t <= 0){
+				time = final;
+				run = false;
+			}
+		}
+	}
+
+}
